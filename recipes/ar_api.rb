06b2a8f0f3d59d32d54b8733b7780e8a762251de
euonymus <euonymus0220@gmail.com>
2018-03-12 14:57:05 +0900

    apache docroot

--- recipes/ar_api.rb
+++ recipes/ar_api.rb
@@ -1,1 +1,15 @@
 pip_requirements "#{node[:ar_api][:app_root]}/requirements.txt"
+
+# delete a uploads directory
+directory node[:ar_api][:app_root] + "/static/uploads" do
+  recursive true
+  action :delete
+end
+
+# create a uploads directory
+directory node[:ar_api][:app_root] + '/static/uploads' do
+  owner "www-data"
+  group "www-data"
+  mode "0775"
+  action :create
+end
